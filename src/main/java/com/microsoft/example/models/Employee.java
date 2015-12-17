91a337dbe1e8d80d6eb443785cef016921549cd7
Ted Neward <ted@tedneward.com>
2015-12-16 22:56:44 -0800

    Added in fee-calculation logic and associated tests

--- src/main/java/com/microsoft/example/models/Employee.java
+++ src/main/java/com/microsoft/example/models/Employee.java
@@ -1,5 +1,6 @@
 package com.microsoft.example.models;
 
+import java.util.List;
 
 /**
  * Simple model basically mimicking what's in the database
@@ -21,6 +22,19 @@ public class Employee
 	
 	public String getPassword() { return password; }
 	public void setPassword(String value) { password = value; }
+  
+  public int getTotalFees(List<Fare> fares) {
+    int total = 0;
+    
+    for (Fare f : fares) {
+      total += f.getDriverFee();
+    }
+    
+    return total;
+  }
+  public float getTotalFeesInDollars(List<Fare> fares) {
+    return getTotalFees(fares) / 100.0f;
+  }
 	
 	@Override
 	public String toString() {
